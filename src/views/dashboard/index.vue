00d5c5fff18341abe2ea9f9e4551690e9af58eca
SuperYing <13975145507@163.com>
2023-06-17 22:10:08 +0800

    feat: add temp views

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="dashboard">
-    <el-button>{{ text }}</el-button>
+    <el-button @click="handleClick">{{ text }}</el-button>
   </div>
 </template>
 <script setup lang="ts">
 const text = ref('我是主页')
+
+const _router = useRouter()
+
+const handleClick = () => {
+  _router.push('/system-setting/user-manage')
+}
 </script>
---
+++ src/views/system-setting/user-manage/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="menu-manage">用户管理</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
