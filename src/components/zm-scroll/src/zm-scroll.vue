a2ddcc6a21b28731a32903752929a7eebe7dabd1
SuperYing <13975145507@163.com>
2023-06-30 16:20:21 +0800

    fix: zm-scroll 问题修复

--- src/components/zm-scroll/src/zm-scroll.vue
+++ src/components/zm-scroll/src/zm-scroll.vue
@@ -3,8 +3,20 @@
 -->
 <template>
   <div class="zm-scroll">
-    <div ref="wrapperRef" class="zm-scroll__wrapper">
-      <div ref="contentRef" class="zm-scroll__content">
+    <div
+      ref="wrapperRef"
+      :class="{
+        'zm-scroll__wrapper': true,
+        'zm-scroll__wrapper--scrollX': scrollX
+      }"
+    >
+      <div
+        ref="contentRef"
+        :class="{
+          'zm-scroll__content': true,
+          'zm-scroll__content--scrollX': scrollX
+        }"
+      >
         <slot />
       </div>
     </div>
@@ -25,18 +37,18 @@ interface Props {
 
 const props = defineProps<Props>()
 
+const scrollX = computed(() => props.options.scrollX)
+
 // 容器元素
 const wrapperRef = ref<HTMLElement>()
 const contentRef = ref<HTMLElement>()
 // 滚动实例
 const instance = ref<BS>()
 
-const { width: wrapperWidth, height: wrapperHeight } = useElementSize(
-  wrapperRef.value
-)
-const { width: contentWidth, height: contentHeight } = useElementSize(
-  contentRef.value
-)
+const { width: wrapperWidth, height: wrapperHeight } =
+  useElementSize(wrapperRef)
+const { width: contentWidth, height: contentHeight } =
+  useElementSize(contentRef)
 
 watch([wrapperWidth, wrapperHeight, contentWidth, contentHeight], () => {
   // 容器元素和内容元素发生变化，刷新 BS
@@ -45,7 +57,7 @@ watch([wrapperWidth, wrapperHeight, contentWidth, contentHeight], () => {
 
 function init() {
   if (!wrapperRef.value) return
-  instance.value = new BS(wrapperRef.value, props)
+  instance.value = new BS(wrapperRef.value, props.options)
 }
 
 onMounted(() => {
@@ -56,3 +68,6 @@ defineExpose({
   instance
 })
 </script>
+<style lang="scss" scoped>
+@import '../style/zm-scroll.scss';
+</style>
