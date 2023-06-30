2ccbf6841f0b39101d836b8e69cd5647b3137520
SuperYing <13975145507@163.com>
2023-06-30 14:47:25 +0800

    feat: zm-utils add WithNever type

--- packages/zimu-utils/src/ts-types/index.ts
+++ packages/zimu-utils/src/ts-types/index.ts
@@ -1,11 +1,11 @@
 // T 或 空类型
 export type Nullable<T> = T | null
 
+// T 或 never
+export type WithNever<T> = T | never
+
 // 获取 T 对象的值类型
 export type ValueOf<T> = T[keyof T]
-
-// 返回 Promise 类型的函数
-export type WithPromise<T> = () => Promise<T>
 
 // 懒加载
 export type Lazy<T> = () => Promise<T>
