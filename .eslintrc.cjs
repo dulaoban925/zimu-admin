55a25f320875542652e54ea9ba27908b454ecc58
SuperYing <13975145507@163.com>
2023-07-03 16:21:14 +0800

    chore: eslint config 调整

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -23,6 +23,7 @@ module.exports = {
   rules: {
     'vue/multi-word-component-names': 0,
     'vue/no-v-model-argument': 0,
-    'vue/no-multiple-template-root': 0
+    'vue/no-multiple-template-root': 0,
+    'no-void': 0
   }
 }
