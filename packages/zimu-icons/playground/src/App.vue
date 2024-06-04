bd8dda3b60c8dcc1d77cfa3644bf2b9744c69bfc
SuperYing <13975145507@163.com>
2024-06-04 09:15:53 +0800

    feat: zimu-icon playground

--- packages/zimu-icons/playground/src/App.vue
+++ packages/zimu-icons/playground/src/App.vue
@@ -1,7 +1,12 @@
 <template>
-  <div></div>
+  <h1>我是粉色小熊：</h1>
+  <pink-bear />
+  <h1>我是所有图标：</h1>
+  <component v-for="icon in icons" :key="icon.name" :is="icon"></component>
 </template>
 
-<script setup></script>
+<script setup>
+import { icons } from '@zimu/icons'
+</script>
 
 <style scoped></style>
--- packages/zimu-icons/src/index.ts
+++ packages/zimu-icons/src/index.ts
@@ -1,12 +1,11 @@
-import { App } from 'vue'
-
 import * as icons from './components'
+import type { App } from 'vue'
 
 export interface Options {
   prefix?: string
 }
 
-export default (app: App<Element>, { prefix = '' }: Options = {}) => {
+export default (app: App, { prefix = '' }: Options = {}) => {
   Object.entries(icons).forEach(([k, component]) => {
     app.component(`${prefix}${k}`, component)
   })
