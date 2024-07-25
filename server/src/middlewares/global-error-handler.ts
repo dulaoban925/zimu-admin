8faa22d1d5506ee9f796546f01ec8e182729c39c
SuperYing <13975145507@163.com>
2024-07-25 22:36:56 +0800

    chore(server): test

---
+++ server/src/middlewares/global-error-handler.ts
@@ -0,0 +1,8 @@
+import { ExpressErrorMiddlewareInterface } from 'routing-controllers'
+
+export class GlobalErrorHandler implements ExpressErrorMiddlewareInterface {
+  error(error: any, request: any, response: any, next: (err: any) => any) {
+    console.log('🚀 ~ GlobalErrorHandler ~ error:', error)
+    next(error)
+  }
+}
