5b16bb05e33aa451bfb0d92f85f090eef4f19823
SuperYing <13975145507@163.com>
2024-07-25 22:15:13 +0800

    refactor(server): 调整枚举值

--- server/src/constants/enums/menu.ts
+++ server/src/constants/enums/menu.ts
@@ -7,8 +7,14 @@
  * BUTTON - 功能按钮
  * PAGE - 关联页面，如详情，查看等
  */
-export enum MENU_TYPE {
+export const enum MENU_TYPE {
   MENU = 'menu', // 菜单
   BUTTON = 'button', // 按钮
   PAGE = 'page' // 系统页
+}
+
+export const enum MENU_TYPE_DESC {
+  menu = '菜单', // 菜单
+  button = '按钮', // 按钮
+  page = '系统页' // 系统页
 }
--- server/src/constants/enums/status.ts
+++ server/src/constants/enums/status.ts
@@ -4,13 +4,23 @@
  * ACTIVATE - 激活
  * DEACTIVATE - 停用
  */
-export enum ACTIVATION_STATUS {
+export const enum ACTIVATION_STATUS {
   ACTIVATED = 'activated',
   DEACTIVATED = 'deactivated'
 }
 
+export const enum ACTIVATION_STATUS_DESC {
+  activated = '激活',
+  deactivated = '停用'
+}
+
 // 用户在职状态
-export enum USER_STATUS {
+export const enum USER_STATUS {
   SERVING = 'serving', // 在职
   RESIGNED = 'resigned' // 离职
+}
+
+export const enum USER_STATUS_DESC {
+  serving = '在职', // 在职
+  resigned = '离职' // 离职
 }
