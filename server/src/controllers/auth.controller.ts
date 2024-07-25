6bf90e261a0f2834c647d48aec53c198e5ec83e7
SuperYing <13975145507@163.com>
2024-07-25 22:41:12 +0800

    feat(server): 新增 queryAuthByUsername 接口雏形

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,13 +1,28 @@
 /**
  * 权限 controller
  */
-import { Controller } from 'routing-controllers'
+import { Authorized, Controller, Get, QueryParam } from 'routing-controllers'
 import { AuthService } from '@services/auth.service'
+import { success } from '@utils/r'
 import { BaseController } from './base/base-controller'
 
 @Controller('/auth')
 export class AuthController extends BaseController {
   constructor() {
     super(new AuthService())
+  }
+
+  /**
+   * 通过用户名获取所有权限，返回菜单、按钮、等所有资源
+   * @param username 用户名
+   */
+  @Get('/userAuth')
+  @Authorized()
+  async queryAuthByUsername(
+    @QueryParam('username', { required: true }) username: string
+  ) {
+    const resources = await this.currentService.queryAuthByUsername(username)
+
+    return success(resources)
   }
 }
--- server/src/services/auth.service.ts
+++ server/src/services/auth.service.ts
@@ -8,4 +8,17 @@ export class AuthService extends BaseService {
   constructor() {
     super(Auth)
   }
+
+  /**
+   * 根据用户名查询所有资源数据
+   * @param username 用户名
+   */
+  async queryAuthByUsername(username: string) {
+    /**
+     * 1. 当前用户绑定的角色
+     * 2. 所有角色关联的权限
+     * 3. 所有权限关联的资源列表
+     */
+    console.log(username)
+  }
 }
