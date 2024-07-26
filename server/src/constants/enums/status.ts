97f7ad449b6e62c5be82f768dbe71f32d9a4dc0d
SuperYing <13975145507@163.com>
2024-07-26 22:07:37 +0800

    perf(server): 调整枚举值

--- server/src/constants/enums/status.ts
+++ server/src/constants/enums/status.ts
@@ -4,23 +4,23 @@
  * ACTIVATE - 激活
  * DEACTIVATE - 停用
  */
-export const enum ACTIVATION_STATUS {
+export enum ACTIVATION_STATUS {
   ACTIVATED = 'activated',
   DEACTIVATED = 'deactivated'
 }
 
-export const enum ACTIVATION_STATUS_DESC {
+export enum ACTIVATION_STATUS_DESC {
   activated = '激活',
   deactivated = '停用'
 }
 
 // 用户在职状态
-export const enum USER_STATUS {
+export enum USER_STATUS {
   SERVING = 'serving', // 在职
   RESIGNED = 'resigned' // 离职
 }
 
-export const enum USER_STATUS_DESC {
+export enum USER_STATUS_DESC {
   serving = '在职', // 在职
   resigned = '离职' // 离职
 }
