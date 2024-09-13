8f031c7a08be0e544caaa557d2283b9a5762925e
SuperYing <13975145507@163.com>
2024-09-13 17:05:42 +0800

    fix(server): 修复 jwt 验证问题

--- server/src/utils/auth-checker.ts
+++ server/src/utils/auth-checker.ts
@@ -6,24 +6,26 @@ import { UserService } from '@services/user.service'
 import jwt from 'jsonwebtoken'
 import type { Action } from 'routing-controllers'
 
-export function authChecker(action: Action) {
+export async function authChecker(action: Action) {
   // 请求头 Authorization 获取 token
   const token = action.request.headers.authorization?.split(' ')[1]
   // 若 token 不存在，不通过
   if (!token) return false
 
-  // jwt 验证 token 是否有效
-  jwt.verify(token, JWT_SECRET, (err: any, payload: any) => {
-    // 若 error 存在，不通过
-    if (err) {
-      return false
-    }
+  try {
+    // jwt 验证 token 是否有效
+    const payload = jwt.verify(token, JWT_SECRET)
 
     // 验证用户名是否有效
-    const username = payload.username
-    const user = new UserService().queryByUsername(username)
-    // 若不存在对应用户，不通过
-    if (!user) return false
-  })
-  return true
+    const username = (payload as jwt.JwtPayload).username
+    if (username) {
+      const user = await new UserService().queryByUsername(username)
+      // 若不存在对应用户，不通过
+      if (user) return true
+    }
+  } catch {
+    return false
+  }
+
+  return false
 }
