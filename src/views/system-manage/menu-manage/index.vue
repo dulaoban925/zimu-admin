d4a27b9eca605d49cf5442c3a14ce174e5a7c109
SuperYing <13975145507@163.com>
2024-10-31 10:45:40 +0800

    feat: 调整菜单跳转类型展示字段

--- src/views/system-manage/menu-manage/index.vue
+++ src/views/system-manage/menu-manage/index.vue
@@ -27,7 +27,11 @@
           <el-icon size="18"><component :is="getIcon(icon)" /></el-icon>
         </template>
       </zm-table-column>
-      <zm-table-column prop="navigateType" label="跳转类型" min-width="100" />
+      <zm-table-column
+        prop="navigateTypeText"
+        label="跳转类型"
+        min-width="100"
+      />
       <zm-table-column prop="path" label="地址 URL" min-width="120" />
       <zm-table-column prop="level" label="层级" min-width="60" />
       <zm-table-column prop="sort" label="排序" min-width="60" />
