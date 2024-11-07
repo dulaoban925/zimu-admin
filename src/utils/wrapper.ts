f6e88856908a019f5ede13b80ce60c43f249038d
SuperYing <13975145507@163.com>
2024-11-07 09:17:56 +0800

    fix: wraper 调整

--- src/utils/wrapper.ts
+++ src/utils/wrapper.ts
@@ -5,8 +5,8 @@
  * promise 包装函数，返回一个 Promise<T>
  * @param fn 被包装额函数，可能为同步或异步
  */
-export function promiseWrapper(fn: (...args: any) => any, context?: any) {
-  return function (...args: any) {
+export function promiseWrapper(fn: (...args: any[]) => any, context?: any) {
+  return function (...args: any[]) {
     return new Promise((resolve, reject) => {
       tryCatchWrapper(
         fn,
@@ -21,11 +21,11 @@ export function promiseWrapper(fn: (...args: any) => any, context?: any) {
 /**
  * try ... catch ... 包装器
  */
-export function tryCatchWrapper(fn: (...args: any) => any, context?: any) {
-  return function (...args: any) {
+export function tryCatchWrapper(fn: (...args: any[]) => any, context?: any) {
+  return function (...args: any[]) {
     return new Promise((resolve, reject) => {
       try {
-        const result = context ? fn.apply(context, ...args) : fn(...args)
+        const result = context ? fn.apply(context, args) : fn(...args)
         if (result instanceof Promise) {
           result.then(resolve).catch(reject)
         } else {
